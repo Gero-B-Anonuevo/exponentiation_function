6beef9117456935b9a52c54bd0726ebbc58a1456
Anonuevo <geroanonuevo3@gmail.com>
2024-01-22 23:00:35 +0800

    checked the recommended solution, the recommended solution cannot produce the expanded form included in the expected outcome

--- prog.py
+++ prog.py
@@ -9,11 +9,5 @@ def exponent(base, exp):
     print("base = ", base)
     print("exponent = ", exp)
     end_product = base ** exp
-    ender = = end_product
     print(base, " raises to the power of ", exp, " is: ", end_product)
-    for num in range(exponent_number):
-        num = base_number
-        print(num, end='  *')
-    return ender
-
 print(exponent(base_number, exponent_number))
